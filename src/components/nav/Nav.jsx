fc68970f783f7478d544e0e55fe45e61cf2e7c49
Arun Nair <davor.suker09@gmail.com>
2023-02-13 08:34:41 -0600

    buttons

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import './nav.css'
+import { AiOutlineHome, AiOutlineUser, AiOutlineContacts } from 'react-icons/ai'
+import { GiBookCover } from 'react-icons/gi'
+import {RiServiceLine} from 'react-icons/ri'
+
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <nav>
+      <a href='#'><AiOutlineHome /></a>
+      <a href='#about'><AiOutlineUser /></a>
+      <a href='#experience'><GiBookCover /></a>
+      <a href='#services'><RiServiceLine /></a>
+      <a href='#contact'><AiOutlineContacts/></a>
+    </nav> 
   )
 }
 
